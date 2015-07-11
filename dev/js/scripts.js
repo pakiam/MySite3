caf634ee60f297458c9b433e803765d8fa75493b
Maksim <akiam@mail.ru>
2015-07-11 21:25:02 +0300

    Added effects for nav bar(header) plus sliders fixes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,22 +1,4 @@
 $(document).ready(function() {
-    $(".slider-header").each(function () { // обрабатываем каждый слайдер
-        var obj = $(this);
-        $(obj).append("<div class='nav'></div>");
-        $(obj).find("li").each(function () {
-            $(obj).find(".nav").append("<span rel='"+$(this).index()+"'></span>"); // добавляем блок навигации
-            $(this).addClass("slider-header"+$(this).index());
-        });
-        $(obj).find(".nav span").first().addClass("on"); // делаем активным первый элемент меню
-    });
-    $(".slider").each(function () { // обрабатываем каждый слайдер
-        var obj = $(this);
-        $(obj).append("<div class='nav1'></div>");
-        $(obj).find("li").each(function () {
-            $(obj).find(".nav1").append("<span rel='"+$(this).index()+"'></span>"); // добавляем блок навигации
-            $(this).addClass("slider"+$(this).index());
-        });
-        $(obj).find(".nav1 span").first().addClass("on"); // делаем активным первый элемент меню
-    });
     // hover для блока gallery
     $(".gallery .preview").each(function () {
         var obj=$(this);
@@ -32,32 +14,73 @@ $(document).ready(function() {
             );
         });
     });
+
+
+    //slider-header
+    $(".slider-header").each(function () { // обрабатываем каждый слайдер
+        var obj = $(this);
+        $(obj).append("<div class='nav-slider-header'></div>");
+        $(obj).find(".slide").each(function () {
+            $(obj).find(".nav-slider-header").append("<span rel='"+$(this).index()+"'></span>"); // добавляем блок навигации
+            $(this).addClass("slide"+$(this).index());
+        });
+        $(obj).find(".nav-slider-header span").first().addClass("on"); // делаем активным первый элемент меню
+    });
+
+
+    //slider-quotes
+    $(".slider-quotes").each(function () {
+        var obj = $(this);
+        $(obj).append("<div class='nav-slider-quotes'></div>");
+        $(obj).find(".slide1").each(function () {
+            $(obj).find(".nav-slider-quotes").append("<span rel='"+$(this).index()+"'></span>"); // добавляем блок навигации
+            $(this).addClass("slide1"+$(this).index());
+        });
+        $(obj).find(".nav-slider-quotes span").first().addClass("on"); // делаем активным первый элемент меню
+    });
+    scrollHeader();
 });
-function sliderJS (obj, sl) { // slider function
-    var ul = $(sl).find("ul"); // находим блок
-    var bl = $(sl).find("li.slider-header"+obj); // находим любой из элементов блока
+
+
+
+//slider-header func
+function sliderHeader(obj,sl){
+    var ul = $(sl).find(".slider-header-wrapper"); // находим блок
+    var bl = $(sl).find(".slide"+obj); // находим любой из элементов блока
     var step = $(bl).width(); // ширина объекта
     $(ul).animate({marginLeft: "-"+step*obj}, 500); // 500 это скорость перемотки
 }
-function sliderJS1 (obj, sl) { // slider function
-    var ul = $(sl).find("ul"); // находим блок
-    var bl = $(sl).find("li.slider"+obj); // находим любой из элементов блока
-    var step = $(bl).width(); // ширина объекта
-    $(ul).animate({marginLeft: "-"+step*obj}, 500); // 500 это скорость перемотки
-}
-$(document).on("click", ".slider-header .nav span", function() { // slider click navigate
-    var sl = $(this).closest(".slider-header"); // находим, в каком блоке был клик
-    $(sl).find("span").removeClass("on"); // убираем активный элемент
-    $(this).addClass("on"); // делаем активным текущий
-    var obj = $(this).attr("rel"); // узнаем его номер
-    sliderJS(obj, sl); // слайдим
+
+$(document).on("click", ".slider-header span",function(){
+   var sl=$(this).closest(".slider-header");
+    $(sl).find("span").removeClass("on");
+    $(this).addClass("on");
+    var obj=$(this).attr("rel");
+    sliderHeader(obj, sl);
     return false;
 });
-$(document).on("click", ".slider .nav1 span", function() { // slider click navigate
-    var sl = $(this).closest(".slider"); // находим, в каком блоке был клик
-    $(sl).find("span").removeClass("on"); // убираем активный элемент
-    $(this).addClass("on"); // делаем активным текущий
-    var obj = $(this).attr("rel"); // узнаем его номер
-    sliderJS1(obj, sl); // слайдим
-    return false;
+
+//slider-quotes func
+    function sliderQuotes(obj,sl){
+        var ul = $(sl).find(".slider-quotes-wrapper"); // находим блок
+        var bl = $(sl).find(".slide1"+obj); // находим любой из элементов блока
+        var step = $(bl).width(); // ширина объекта
+        $(ul).animate({marginLeft: "-"+step*obj}, 500); // 500 это скорость перемотки
+    }
+
+    $(document).on("click", ".slider-quotes span",function(){
+        var sl=$(this).closest(".slider-quotes");
+        $(sl).find("span").removeClass("on");
+        $(this).addClass("on");
+        var obj=$(this).attr("rel");
+        sliderQuotes(obj, sl);
+        return false;
+    });
+
+$(document).scroll(function(){
+   if( $(this).scrollTop()>75){
+       $("header").css("background-color", "#775B83");
+   }else{
+       $("header").css("background-color", "rgba(151, 125, 166, .5)");
+   }
 });
